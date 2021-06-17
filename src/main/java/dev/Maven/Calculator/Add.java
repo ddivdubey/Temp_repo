6b133ee7fe8d77d5343b2bf9f1fe6ffb0185cf3a
ddivdubey <divyanshdubey900@gmail.com>
2021-06-17 12:52:08 +0530

    Folder1 first commit

---
+++ src/main/java/dev/Maven/Calculator/Add.java
@@ -0,0 +1,7 @@
+package dev.Maven.Calculator;
+
+public class Add {
+	public int a = 10;
+	public int b = 20;
+	public int c = a+b;
+}
---
+++ src/main/java/dev/Maven/Calculator/Difference.java
@@ -0,0 +1,8 @@
+package dev.Maven.Calculator;
+
+public class Difference {
+
+	public int a = 100;
+	public int b = 20;
+	public int c = a-b;
+}
